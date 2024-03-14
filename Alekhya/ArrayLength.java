ec767fd2dfe6b8af6f16790e99be77eac554d485
Alekhya-WSU <kondrakunta.3@wright.edu>
2024-03-13 22:36:23 -0400

    Alekhya_test
    Alekhya_test

---
+++ Alekhya/ArrayLength.java
@@ -0,0 +1,11 @@
+
+public class ArrayLength {
+
+	public static void main(String[] args) {
+		int[]Array = {4,6,9,6};
+		int arraylength = Array.length;
+		System.out.println(" length of the array : "+ arraylength );
+		
+	}
+
+}
